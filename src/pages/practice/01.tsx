ade7d29da8d5d1fe9f9d2141e2051a33e867ddff
zetton110 <locolv.kohe@gmail.com>
2025-01-14 12:23:39 +0900

    P01 （余談）コンポーネント呼び出しの別の呼び出し方

--- src/pages/practice/01.tsx
+++ src/pages/practice/01.tsx
@@ -13,6 +13,17 @@ const Page: NextPage = () => {
         title3="This is title"
         content3="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur"
       />
+      {/* {ThreeBoxContent({
+        title1: 'This is title',
+        content1:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur',
+        title2: 'This is title',
+        content2:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur',
+        title3: 'This is title',
+        content3:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur',
+      })} */}
     </div>
   );
 };

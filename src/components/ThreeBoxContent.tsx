943f37598fbaaff99504e4cafa58f253527046b9
zetton110 <locolv.kohe@gmail.com>
2025-01-14 12:02:34 +0900

    P01 コンポーネントを分割する

--- src/components/ThreeBoxContent.tsx
+++ src/components/ThreeBoxContent.tsx
@@ -1,4 +1,25 @@
 const ThreeBoxContent = (): JSX.Element => {
-  return <></>;
+  return (
+    <div className="grid grid-cols-3 gap-x-4">
+      <div className="p-6 shadow-xl">
+        <h3 className="mt-4 text-base">This is title</h3>
+        <p className="mt-4 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+        </p>
+      </div>
+      <div className="p-6 shadow-xl">
+        <h3 className="mt-4 text-base">This is title</h3>
+        <p className="mt-4 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+        </p>
+      </div>
+      <div className="p-6 shadow-xl">
+        <h3 className="mt-4 text-base">This is title</h3>
+        <p className="mt-4 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+        </p>
+      </div>
+    </div>
+  );
 };
 export default ThreeBoxContent;
--- src/pages/practice/01.tsx
+++ src/pages/practice/01.tsx
@@ -1,28 +1,11 @@
 import { NextPage } from 'next';
 
+import ThreeBoxContent from '@/components/ThreeBoxContent';
+
 const Page: NextPage = () => {
   return (
     <div className="mx-auto mt-10 max-w-4xl">
-      <div className="grid grid-cols-3 gap-x-4">
-        <div className="p-6 shadow-xl">
-          <h3 className="mt-4 text-base">This is title</h3>
-          <p className="mt-4 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-          </p>
-        </div>
-        <div className="p-6 shadow-xl">
-          <h3 className="mt-4 text-base">This is title</h3>
-          <p className="mt-4 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-          </p>
-        </div>
-        <div className="p-6 shadow-xl">
-          <h3 className="mt-4 text-base">This is title</h3>
-          <p className="mt-4 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-          </p>
-        </div>
-      </div>
+      <ThreeBoxContent />
     </div>
   );
 };

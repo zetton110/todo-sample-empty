5d64cee7c4c14bcf3c0680b5abbe18334f97e4c8
zetton110 <locolv.kohe@gmail.com>
2025-01-14 15:38:02 +0900

    P03 リファクタリング

--- src/pages/practice/03.tsx
+++ src/pages/practice/03.tsx
@@ -3,17 +3,16 @@ import { useState } from 'react';
 
 const Page: NextPage = () => {
   const [text, setText] = useState('');
+  const hanbdleChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
   return (
     <div className="mx-auto mt-10 max-w-4xl">
       <div className="flex justify-center">
         <div>
           <h2 className="mb-4 text-center text-6xl">{text}</h2>
           <div className="flex justify-center">
-            <input
-              onChange={(e) => setText(e.target.value)}
-              className="border px-3 py-2"
-              type="text"
-            />
+            <input onChange={hanbdleChangeText} className="border px-3 py-2" type="text" />
           </div>
         </div>
       </div>

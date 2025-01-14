d7467fcce718e5f09885d233e78bd4c4ddc5e89f
zetton110 <locolv.kohe@gmail.com>
2025-01-14 15:37:11 +0900

    P03 useStateの適用

--- src/pages/practice/03.tsx
+++ src/pages/practice/03.tsx
@@ -1,13 +1,19 @@
 import { NextPage } from 'next';
+import { useState } from 'react';
 
 const Page: NextPage = () => {
+  const [text, setText] = useState('');
   return (
     <div className="mx-auto mt-10 max-w-4xl">
       <div className="flex justify-center">
         <div>
-          <h2 className="mb-4 text-center text-6xl">こんにちは</h2>
+          <h2 className="mb-4 text-center text-6xl">{text}</h2>
           <div className="flex justify-center">
-            <input className="border px-3 py-2" type="text" />
+            <input
+              onChange={(e) => setText(e.target.value)}
+              className="border px-3 py-2"
+              type="text"
+            />
           </div>
         </div>
       </div>

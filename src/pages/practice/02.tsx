d001d4c41a1b8292eedb885ffd65b434f95033a8
zetton110 <locolv.kohe@gmail.com>
2025-01-14 12:43:55 +0900

    P02 とりあえず動くところまでコーディング

--- src/pages/practice/02.tsx
+++ src/pages/practice/02.tsx
@@ -1,7 +1,25 @@
 import { NextPage } from 'next';
+import { useState } from 'react';
+
+import Button from '@/components/common/parts/Button';
 
 const Page: NextPage = () => {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="mx-auto mt-10 max-w-4xl">
+      <div className="flex justify-center">
+        <div>
+          <h2 className="mb-4 text-center text-6xl">{count}</h2>
+          <Button
+            onClick={() => setCount((prevState) => prevState + 1)}
+            label="カウントアップ"
+            variant="primary"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;

02bbc9342be44bea0082ffa3b8a817d5947cae0a
zetton110 <locolv.kohe@gmail.com>
2025-01-14 12:21:47 +0900

    P01 props で内容を渡すようにする

--- src/components/ThreeBoxContent.tsx
+++ src/components/ThreeBoxContent.tsx
@@ -1,23 +1,27 @@
-const ThreeBoxContent = (): JSX.Element => {
+type Props = {
+  title1: string;
+  content1: string;
+  title2: string;
+  content2: string;
+  title3: string;
+  content3: string;
+};
+
+const ThreeBoxContent = (props: Props): JSX.Element => {
+  const { title1, content1, title2, content2, title3, content3 } = props;
   return (
     <div className="grid grid-cols-3 gap-x-4">
       <div className="p-6 shadow-xl">
-        <h3 className="mt-4 text-base">This is title</h3>
-        <p className="mt-4 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        </p>
+        <h3 className="mt-4 text-base">{title1}</h3>
+        <p className="mt-4 text-base">{content1}</p>
       </div>
       <div className="p-6 shadow-xl">
-        <h3 className="mt-4 text-base">This is title</h3>
-        <p className="mt-4 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        </p>
+        <h3 className="mt-4 text-base">{title2}</h3>
+        <p className="mt-4 text-base">{content2}</p>
       </div>
       <div className="p-6 shadow-xl">
-        <h3 className="mt-4 text-base">This is title</h3>
-        <p className="mt-4 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        </p>
+        <h3 className="mt-4 text-base">{title3}</h3>
+        <p className="mt-4 text-base">{content3}</p>
       </div>
     </div>
   );
--- src/pages/practice/01.tsx
+++ src/pages/practice/01.tsx
@@ -5,7 +5,14 @@ import ThreeBoxContent from '@/components/ThreeBoxContent';
 const Page: NextPage = () => {
   return (
     <div className="mx-auto mt-10 max-w-4xl">
-      <ThreeBoxContent />
+      <ThreeBoxContent
+        title1="This is title"
+        content1="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur"
+        title2="This is title"
+        content2="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur"
+        title3="This is title"
+        content3="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur"
+      />
     </div>
   );
 };

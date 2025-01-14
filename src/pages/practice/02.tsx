4027e0c9ad3a65193d9be027fac004cfc0a93bc4
zetton110 <locolv.kohe@gmail.com>
2025-01-14 12:50:02 +0900

    P02  リファクタリング

--- src/pages/practice/02.tsx
+++ src/pages/practice/02.tsx
@@ -6,16 +6,16 @@ import Button from '@/components/common/parts/Button';
 const Page: NextPage = () => {
   const [count, setCount] = useState(0);
 
+  const onClickCountUp = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
   return (
     <div className="mx-auto mt-10 max-w-4xl">
       <div className="flex justify-center">
         <div>
           <h2 className="mb-4 text-center text-6xl">{count}</h2>
-          <Button
-            onClick={() => setCount((prevState) => prevState + 1)}
-            label="カウントアップ"
-            variant="primary"
-          />
+          <Button onClick={onClickCountUp} label="カウントアップ" variant="primary" />
         </div>
       </div>
     </div>

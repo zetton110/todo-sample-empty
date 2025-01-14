9b7ab40f8ecd9e4e2745d537a312f559ed7a4acf
zetton110 <locolv.kohe@gmail.com>
2025-01-14 16:57:51 +0900

    P04 とりあえず動くところまでコーディング

--- src/pages/practice/04.tsx
+++ src/pages/practice/04.tsx
@@ -1,16 +1,25 @@
 import { NextPage } from 'next';
+import { useState } from 'react';
 
 import Button from '@/components/common/parts/Button';
 
 const Page: NextPage = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const handleClickShown = () => {
+    setIsHidden(false);
+  };
+  const handleClickHidden = () => {
+    setIsHidden(true);
+  };
+
   return (
     <div className="mx-auto mt-10 max-w-4xl">
       <div className="flex justify-center">
         <div>
-          <h2 className="mb-4 text-6xl">こんにちは</h2>
+          {!isHidden && <h2 className="mb-4 text-6xl">こんにちは</h2>}
           <div className="flex justify-center gap-x-4">
-            <Button label="表示" variant="primary" />
-            <Button label="非表示" variant="primary" />
+            <Button onClick={handleClickShown} label="表示" variant="primary" />
+            <Button onClick={handleClickHidden} label="非表示" variant="primary" />
           </div>
         </div>
       </div>

aefc99d0e1aec50cb241c3d855024ba94055910a
zetton110 <locolv.kohe@gmail.com>
2025-01-16 01:29:35 +0900

    P06 とりあえず動くところまでベタ書きでコーディング

--- src/pages/practice/06.tsx
+++ src/pages/practice/06.tsx
@@ -1,8 +1,21 @@
 import { NextPage } from 'next';
+import { ChangeEvent, useState } from 'react';
 
 import Button from '@/components/common/parts/Button';
 
 const Page: NextPage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const [feedbackList, setFeedbackList] = useState<string[]>([]);
+  const handleSubmit = () => {
+    if (!inputValue.trim()) return;
+    setFeedbackList((prevState) => [...prevState, inputValue]);
+    setInputValue('');
+  };
+
   return (
     <div className="mx-auto mt-8 max-w-4xl">
       <div className="flex justify-center">
@@ -10,12 +23,16 @@ const Page: NextPage = () => {
           <textarea
             className="rounded-sm border px-3 py-2"
             placeholder="フィードバックを入力してください"
+            value={inputValue}
+            onChange={handleInputChange}
           />
           <div className="flex justify-center">
-            <Button label="送信する" variant="primary"></Button>
+            <Button label="送信する" variant="primary" onClick={handleSubmit} />
           </div>
           <div>
-            <li>フィードバックです</li>
+            {feedbackList.map((feedback, index) => (
+              <li key={index}>{feedback}</li>
+            ))}
           </div>
         </div>
       </div>
